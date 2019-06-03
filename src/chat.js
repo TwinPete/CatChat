4a2008e7c0719d68c65a3de5bb2d6252ac66120b
Peter Mayrguendter <petermayrguendter@ymail.com>
2019-06-03 21:11:06 +0200

    task(chat): basic styling

--- src/chat.js
+++ src/chat.js
@@ -6,30 +6,55 @@ import $ from "jquery";
 require('./views/audio/meow.mp3');
 
 
+
 navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 navigator.getMedia({video: true, audio: false}, function(stream){
 
 
     let vendorUrl = window.URL || window.webkitURL;
 
-   localStorage.setItem('videoInit', true);
 
-   
-    
-    $('#myId').on('click', function(){
-        localStorage.setItem('init', 'true');
-        
-       let Peer = require('simple-peer');
-       let peer = new Peer({
-       initiator: localStorage.getItem('init') === 'true',
-       trickle: false,
-       stream: stream
-   });
+    $('.chat__videoIcon').on('click', function(){
+
+        // create new Peer Object
+
+        // Create Code
+
+        // push code to database
 
     });
 
+    // Wenn callRequest einen zusätzlichen Eintrag bekommt,
+    // und der Eintrag an den anderen User adressiert ist
+    // wandle Code um und generiere den Accept Code.
+    // Finally push Code as callAccepted to database
+
+    // Wenn callAccept einen zusätzlichen Eintrag bekommt,
+    // und der Eintrag an mich selbst referenziert ist
+    // wandle Code um und verbinde.
+
+    
+
+        // localStorage.setItem('init', 'true');
+        
+       var Peer = require('simple-peer');
+       var peer = new Peer({
+       initiator: localStorage.getItem('init') === 'true',
+       trickle: false,
+       stream: stream
+       });
+
+       $('.chat__header').on('click', function(){
+          localStorage.setItem('init', 'true');
+
+
+       });
+
+
+
     peer.on('signal', function(data){
         console.log('scnd hit');
+        alert(data);
         $('.myId').text(JSON.stringify(data));
     });
 
@@ -55,7 +80,7 @@ navigator.getMedia({video: true, audio: false}, function(stream){
 
     peer.on('stream', function(stream){
             let video = document.createElement('video');
-            $('.video').append(video);
+            $('.videochat__body').append(video);
             
             console.log(stream);
 
