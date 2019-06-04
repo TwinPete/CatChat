f61591b3aac00e2ca016c27d87969ea856a4ca88
Peter Mayrguendter <petermayrguendter@ymail.com>
2019-06-05 00:40:53 +0200

    task(videochat & Messanging): more functions for videochat and basic messanging

--- src/chat.js
+++ src/chat.js
@@ -6,6 +6,7 @@ require('./views/audio/meow.mp3');
 import firebase from 'firebase/app';
 import 'firebase/database';
 import { firebaseConfig } from "./modules/firebase.js"
+require('./views/audio/ringtone.mp3');
 
 
 
@@ -23,7 +24,11 @@ navigator.getMedia({video: true, audio: false}, function(stream){
     let contacts = database.ref(username + '/contacts');
     let callRequest = database.ref(username + '/call--request');
     let callAccept = database.ref(username +'/call--accept');
+    let callRefused = database.ref(username +'/call--refused');
+    let callCanceled = database.ref(username +'/call--canceled');
+    let callTimeout = database.ref(username +'/call--timeout');
     let vendorUrl = window.URL || window.webkitURL;
+    let ringtone = new Audio('./res/ringtone.mp3');
 
     initializeUI();
 
@@ -31,7 +36,7 @@ navigator.getMedia({video: true, audio: false}, function(stream){
 
     function initializeUI(){
         // $('.sidebar__username').text(username);
-        contacts.on('value', function(data){
+        users.on('value', function(data){
             let vals = data.val();
 
             if(vals != null){
@@ -43,24 +48,145 @@ navigator.getMedia({video: true, audio: false}, function(stream){
                     let username = vals[key].username;
                     $('.contacts__list').append('<div class="contacts__contact"><div class="contacts__userpic"></div><div class="contacts__username" data-username="'+ username +'">' + username + '</div></div>');
                 });
+
+                $('.contacts__contact').on('click', function(){
+                    let contact = $(this).find('.contacts__username').attr('data-username');
+                    $('.chat__username').text(contact);
+                    $('.chat__username').attr('data-username', contact);
+
+                    let messages = database.ref('messages/' + username + '--' + contact);
+                    let messagesSwitch = database.ref('messages/' + contact + '--' + username);
+
+                    messages.on('value', function(data) {
+                        let type = true;
+                        console.log('in der message on');
+                        console.log(data.val());
+                        if (data.val() !== null && data.val() !== undefined)
+                        {
+                            localStorage.setItem('message-direction', 'first');
+                            loadMessages(data, type);
+                            console.log('load funktion wird aufgerufen');
+                        }else
+                            {
+                            messagesSwitch.on('value', function(data){
+                                console.log('in der messageSwwitch on');
+                                console.log(data.val());
+                                let type = false;
+
+
+                                if(data.val() !== null && data.val() !== undefined)
+                                {
+                                    localStorage.setItem('message-direction', 'second');
+                                    loadMessages(data, type);
+                                    console.log('load funktion wird aufgerufen');
+                                }else{
+                                    localStorage.setItem('message-direction', 'first');
+                                }
+
+                            }, errData);
+
+                        }
+                    }, errData);
+                });
             }
         }, errData());
     }
-    $('.contacts__contact').on('click', function(){
-        console.log('fgh');
-       let username = $(this).find('contacts__username').attr('data-username');
-        alert(username);
-       $('.chat__username').text(username);
+
+    function loadMessages(data, type){
+
+        $('.message').remove();
+
+
+
+        let messages = data.val();
+        let keys = Object.keys(messages);
+
+        keys.forEach(function(key){
+            let text = messages[key].text;
+            let from = messages[key].from;
+            let date = messages[key].date;
+            let time = messages[key].time;
+            let type= 'message';
+            if(from == username){
+                type = 'message message--ownMessage';
+            }
+
+            $('.chat__main').append('<div class="'+ type +'">\n' +
+                '<div class="message__body">\n' +
+                '    <div class="message__userpic"></div>\n' +
+                '    <div class="message__text">' + text + '</div>\n' +
+                '</div>\n' +
+                '</div>');
+        });
+
+
+    }
+
+    $('.chat__sendButton').on('click', function(){
+        let text = $('.chat__input').val();
+        let date = new Date();
+        let day = date.getDate();
+        let month = date.getMonth();
+        let year = date.getFullYear();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        let milliseconds = date.getMilliseconds();
+        let contact = $('.chat__username').attr('data-username');
+        let type = localStorage.getItem('message-direction');
+        let message = {
+            from: username,
+            to: contact,
+            text: text,
+            date: day + ':' + month + ':' + year,
+            time: hours + ':' + minutes + ':' + minutes + ':' + milliseconds
+        }
+        if(type){
+            let messages = database.ref('messages/' + username + '--' + contact);
+            messages.push(message);
+            // messages.on('value', pushMessage, errData);
+
+        }else{
+            let messagesSwitch = database.ref('messages/' + contact + '--' + username);
+            messagesSwitch.push(message);
+            // messagesSwitch.on('value', pushMessage, errData);
+        }
+
+
     });
+
+    function pushMessage(data){
+        let values = data.val();
+        let keys = Object.keys(values);
+        console.log('here comes the values');
+        console.log(keys);
+        let key = keys.length - 1;
+        let text = values[key].text;
+        let from = values[key].from;
+        let date = values[key].date;
+        let time = values[key].time;
+        let type= 'message';
+
+        if(from != username){
+            type = 'message message--ownMessage';
+        }
+
+        $('.chat__main').append('<div class="'+ type +'">\n' +
+            '<div class="message__body">\n' +
+            '    <div class="message__userpic"></div>\n' +
+            '    <div class="message__text">' + text + '</div>\n' +
+            '</div>\n' +
+            '</div>');
+    }
+
 
     $('.chat__header').on('click', function(){
 
-        // create new Peer Object
+        $('.videochat').addClass('videochat--open');
 
-        // Create Code
+        let contact = $('.chat__username').text();
+        let callAccepted = false;
 
-        // push code to database
-        alert('geht los');
 
         localStorage.setItem('init', 'true');
 
@@ -77,19 +203,61 @@ navigator.getMedia({video: true, audio: false}, function(stream){
 
             let callRequestData = {
                 code: JSON.stringify(data),
-                from: 'twinPete93',
-                to: 'cuteCat93'
+                from: username,
+                to: contact
             }
 
-            callRequest = database.ref('cuteCat93/call--request');
+
+
+            let string = contact + '/call--request';
+            callRequest = database.ref(string);
 
             callRequest.push(callRequestData);
+
+            // in case the user closes the call window, cancel the call, gets a refusal ...
+            // ... or after 60 seconds doest get an answer the callWindow is shut down
+
+            callRefused.on('value', function(data){
+                if(data.val() != null){
+                    closeCallWindow();
+                    callRefused.remove();
+                    callRequest.remove();
+                }
+            }, errData);
+
+            callTimeout.on('value', function(data){
+                if(data.val() != null){
+                    closeCallWindow();
+                    callTimeout.remove();
+                    callRequest.remove();
+                }
+            }, errData);
+
+            $('.videochat__close').on('click', function(){
+                closeCallWindow();
+                let contactName = $('.chat__username').text();
+                callCanceled = database.ref(contactName +'/call--canceled');
+                let canceled = {
+                    time: '13:55'
+                }
+                callCanceled.push(canceled);
+                callRequest.remove();
+            });
+
+            setTimeout(function(){
+                if(!callAccepted){
+                    closeCallWindow();
+                    callRequest.remove();
+                }
+            }, 60000);
         });
 
 
         callAccept.on('value', gotCallAccept, errData);
 
         function gotCallAccept(data){
+
+            callAccepted = true;
 
             let vals = data.val();
 
@@ -130,8 +298,19 @@ navigator.getMedia({video: true, audio: false}, function(stream){
         console.log('error');
     }
 
+    function closeCallWindow(){
+        $('.videochat').removeClass('videochat--open');
+        $('.videochat').addClass('videochat--close');
+        if($('.videochat__requestWindow').length){
+            $('.videochat__requestWindow').remove();
+        }
+        ringtone.pause();
+        ringtone.currentTime = 0
+    }
+
 
     function gotCallRequest(data){
+
         console.log('hier kommt der Code');
         console.log(data.val());
 
@@ -141,6 +320,7 @@ navigator.getMedia({video: true, audio: false}, function(stream){
             let keys = Object.keys(vals);
             console.log(keys);
             console.log(vals[keys[0]].code);
+            let caller = vals[keys[0]].from;
             let requestCode = vals[keys[0]].code;
 
             var Peer = require('simple-peer');
@@ -154,8 +334,58 @@ navigator.getMedia({video: true, audio: false}, function(stream){
                 '                        <div class="videochat__caller"><div class="videochat__callerPic"></div><div class="videochat__callerName">twinPete93</div></div>\n' +
                 '                        <div class="videochat__requestButtons"><div class="videochat__accept">Accept</div><div class="videochat__refuse">Refuse</div></div></div>\n' +
                 '                    </div>');
+            $('.videochat').addClass('videochat--open');
+
+            ringtone.play();
+
+            // if the call got canceled, the called user doesnt accept, closes the window or after 60 seconds without accept the requestWindow gots closed
+
+            callCanceled.on('value', function(data){
+                if(data.val() != null){
+                    closeCallWindow();
+                    callCanceled.remove();
+                }
+            }, errData);
+
+            $('.videochat__close').on('click', function(){
+                closeCallWindow();
+                callRefused = database.ref(caller + '/call--refused');
+                let refused = {
+                    time: '13:55'
+                }
+                callRefused.push(refused);
+            });
+
+            $('.videochat__refuse').on('click', function(){
+                closeCallWindow();
+                callRefused = database.ref(caller + '/call--refused');
+                let refused = {
+                    time: '13:55'
+                }
+                callRefused.push(refused);
+            });
+
+            setTimeout(function(){
+                if($('.videochat__requestWindow').length){
+                    closeCallWindow();
+                    console.log('caller: ' + caller);
+                    callTimeout = database.ref(caller + '/call--timeout');
+                    let timeout = {
+                        time: '13:55'
+                    }
+                    callTimeout.push(timeout);
+                }
+            }, 60000);
+
+
+
+            // if the user accepts the call
 
             $('.videochat__accept').on('click', function(){
+
+                ringtone.pause();
+                ringtone.currentTime = 0
+
                 let code = JSON.parse(requestCode);
                 peer.signal(code);
 
@@ -163,11 +393,11 @@ navigator.getMedia({video: true, audio: false}, function(stream){
                     console.log('scnd hit');
 
                     let accept = {
-                        from: 'cat',
-                        to: 'twinPete93',
+                        from: username,
+                        to: caller,
                         code: JSON.stringify(data)
                     }
-                    callAccept = database.ref('twinPete93/call--accept');
+                    callAccept = database.ref(caller + '/call--accept');
                     callAccept.push(accept);
                 });
 
@@ -211,28 +441,6 @@ navigator.getMedia({video: true, audio: false}, function(stream){
        });
 
 
-
-
-
-
-    $('.connect').on('click', function(){
-        let data = $('.otherId').val();
-        let otherId = JSON.parse(data);
-        peer.signal(otherId);
-    });
-
-    $('.sendMessage').on('click', function(){
-        let youreMessage = $('.messageText').val();
-        console.log(youreMessage);
-        peer.send(youreMessage);
-    });
-
-    peer.on('data', function(data){
-        console.log('message recieved');
-        let audio = new Audio('./res/meow.mp3');
-        audio.play();
-        $('.message').append(data + '\n');
-    });
 
 
 
